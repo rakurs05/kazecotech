a1e3ab91fd0ea3b3c8736360454f31a811645e9d
rakutinbond@gmail.com <rakutin2005@mail.ru>
2022-04-17 20:09:53 +0600

    awd

--- script/locget.php
+++ script/locget.php
@@ -1,14 +1,23 @@
 <?php
     //Production DataBase
-    $pdb = new SQLite3("kazecotech.db");
+    class PDB extends SQLite3 {
+        function __construct() {
+           $this->open('../kazecotech.db');
+        }
+     }
+    $pdb = new PDB();
     if(!$pdb) {
         exit("Failed to open DB");
     }
     $sqlrequest = "SELECT * FROM markers WHERE (lat < (" . $_GET["lat"] . "+" . $_GET["range"] . ") AND lat > (" . $_GET["lat"] . "-" . $_GET["range"] . ") AND lng < (" . $_GET["lng"] . "+" . $_GET["range"] . ") AND lng > (" . $_GET["lng"] . "-" . $_GET["range"] . ")";
-    $query = sqlite_query($db, $sqlrequest);
+    $query = $pdb->query($sqlrequest);
     // В цикле выведем все полученные данные
-    while ($array = sqlite_fetch_array($query))
+    if(!$query){
+        echo ($_GET["addr"] . " " . $_GET["lat"] . " " . $_GET["lng"]);
+    }
+    while ($array = $query->fetchArray(SQLITE3_ASSOC))
     {
       echo($array['addr']." ".$array['lat']." ".$array['lng']);
     }
+    $pdb->close();
 ?>
